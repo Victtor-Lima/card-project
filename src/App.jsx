0ade10379ed9fe24cf14a16bbb4a2e9e6901079f
João Victor Lima <victtorlima15@gmail.com>
2024-06-06 10:21:12 -0300

    new coffeeOptions were added and JSON was created to store these options

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
-import React, { useState } from 'react'
-import Card from './Card'
+import React from 'react'
 import CardList from './CardList';
-
-const coffeOption = [
-  {
-    img: "../img/img_capuccino.jpg",
-    name: 'Capuccino',
-    price: 10,
-    charact: ['Espresso', 'Milk', 'Classic'],
-    description: 'The traditional preparation involves pouring a shot of espresso into a cup, then adding steamed milk, and topping it with a layer of foamed milk.',
-    id: 'optionCapuccino' 
-  }
-]
+import coffeOptions from '../coffeOption.json'
 
 function App() {
 
   return (
     <>
-      <CardList products={coffeOption} />
+      <CardList products={coffeOptions} />
     </>
   )
 }
